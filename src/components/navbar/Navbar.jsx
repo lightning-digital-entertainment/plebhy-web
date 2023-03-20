8a4c7a75b71ef1f3a8b1fa17d80ead7aa8ec44b2
Egge <80262989+Egge7@users.noreply.github.com>
2023-03-20 13:30:03 +0100

    updated navigation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <div className="flex w-full flex-col border-b-2 border-slate-500 font-montserrat justify-center">
-        <h1 className="text-white text-7xl mx-2 my-2 font-bold text-center">PLEBHY</h1>
-        <ul className="flex flex-row m-2 justify-center">
-            <li className="mx-4 text-lg hover:text-slate-500">Home</li>
-            <li className="mx-4 text-lg hover:text-slate-500">Create</li>
-            <li className="mx-4 text-lg hover:text-slate-500">Library</li>
-        </ul>
-    </div>
-  )
+    return (
+        <div className="flex w-full flex-col border-b-2 border-slate-500 font-montserrat justify-center">
+            <h1 className="text-white text-7xl mx-2 my-2 font-black text-center">
+                PLEBHY
+            </h1>
+            <ul className="flex flex-row m-2 justify-center">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive
+                            ? "mx-4 text-lg text-slate-400 hover:text-slate-500"
+                            : "mx-4 text-lg hover:text-slate-500"
+                    }
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/get-started"
+                    className={({ isActive }) =>
+                        isActive
+                            ? "mx-4 text-lg text-slate-400 hover:text-slate-500"
+                            : "mx-4 text-lg hover:text-slate-500"
+                    }
+                >
+                    Get Started
+                </NavLink>
+            </ul>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/features/home/routes/Home.jsx
+++ src/features/home/routes/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import image from "../../../assets/plebhyScreenshot.png";
 import Button from "../../../components/UI/Button";
 
 function Home() {
+    const navigate = useNavigate();
     return (
         <div className="flex justify-center flex-col mx-2 my-2">
             <h2 className="text-lg font-montserrat text-center">
@@ -11,7 +13,7 @@ function Home() {
             <div className="flex justify-center my-8">
                 <img src={image} alt="screenshot" className="w-72" />
             </div>
-            <Button title='Get Started'/>
+            <Button title='Get Started' onClick={() => {navigate("/get-started")}}/>
         </div>
     );
 }
