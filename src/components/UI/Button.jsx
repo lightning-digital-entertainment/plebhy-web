14ff5f77227928834a86cc8815332651d6299762
Egge <80262989+Egge7@users.noreply.github.com>
2023-03-20 14:04:49 +0100

    changed color scheme

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -5,7 +5,7 @@ function Button({ onClick, title }) {
         <div className="flex justify-center">
             <button
                 onClick={onClick}
-                className="inline px-2 py-2 bg-slate-500 self-start rounded font-montserrat active:bg-slate-700 hover:bg-slate-600"
+                className="inline px-2 py-2 bg-zinc-500 self-start rounded font-montserrat active:bg-zinc-700 hover:bg-zinc-600"
             >
                 {title}
             </button>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
+    const navigate = useNavigate();
     return (
-        <div className="flex w-full flex-col border-b-2 border-slate-500 font-montserrat justify-center">
-            <h1 className="text-white text-7xl mx-2 my-2 font-black text-center">
-                PLEBHY
-            </h1>
+        <div className="flex w-full flex-col border-b-2 border-zinc-500 font-montserrat justify-center">
+            <div
+                onClick={() => {
+                    navigate("/");
+                }}
+            >
+                <h1 className="text-white text-7xl mx-2 my-2 font-black text-center hover:text-zinc-500">
+                    PLEBHY
+                </h1>
+            </div>
             <ul className="flex flex-row m-2 justify-center">
                 <NavLink
                     to="/"
                     className={({ isActive }) =>
                         isActive
-                            ? "mx-4 text-lg text-slate-400 hover:text-slate-500"
-                            : "mx-4 text-lg hover:text-slate-500"
+                            ? "mx-4 text-lg text-zinc-400 hover:text-zinc-500"
+                            : "mx-4 text-lg hover:text-zinc-500"
                     }
                 >
                     Home
@@ -22,8 +29,8 @@ function Navbar() {
                     to="/get-started"
                     className={({ isActive }) =>
                         isActive
-                            ? "mx-4 text-lg text-slate-400 hover:text-slate-500"
-                            : "mx-4 text-lg hover:text-slate-500"
+                            ? "mx-4 text-lg text-zinc-400 hover:text-zinc-500"
+                            : "mx-4 text-lg hover:text-zinc-500"
                     }
                 >
                     Get Started
