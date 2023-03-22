a16dae073369182331e0167156cd6ed06f3980bc
Egge <80262989+Egge7@users.noreply.github.com>
2023-03-22 07:36:20 +0100

    updated get-started

--- src/features/get-started/routes/GetStarted.jsx
+++ src/features/get-started/routes/GetStarted.jsx
@@ -20,7 +20,10 @@ function GetStarted() {
                     >
                         CREATOR
                     </p>
-                    .
+                    . Upload your GIF and include its link as well as the
+                    hashtag <b>#PLEBHY</b> in an event. We will automatically
+                    index your GIF and include a reference to your event so that
+                    others can Zap it.
                 </p>
             </div>
             <div className="bg-zinc-800 p-4 m-4 rounded-lg">
@@ -28,7 +31,10 @@ function GetStarted() {
                     Use a .gif from the PLEBHY library
                 </h2>
                 <p>
-                   You can use PLEBHY gifs directly in clients that integrate the PLEBHY API (here is a list). If your favourite client did not integrate the PLEBHY API yet, you can still use PLEBHY gifs by copying their URL from our web-library.
+                    You can use PLEBHY gifs directly in clients that integrate
+                    the PLEBHY API (here is a list). If your favourite client
+                    did not integrate the PLEBHY API yet, you can still use
+                    PLEBHY gifs by copying their URL from our web-library.
                 </p>
             </div>
         </div>
